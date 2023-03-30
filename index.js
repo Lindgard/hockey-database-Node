271b4faa9129162e96ee006681d020270d450f3f
Lindgard <a.b.lindgard@gmail.com>
2023-03-30 10:53:09 +0200

    began creating endpoints and mock data for testing

--- index.js
+++ index.js
@@ -1,14 +1,36 @@
-const path = require('path');
-const express = require('express');
-const bodyParser = require('body-parser');
+const path = require("path");
+const express = require("express");
+const bodyParser = require("body-parser");
 
 const app = express();
 const port = process.env.PORT || 8080;
 
-app.get('/', (req, res) => {
-    res.send('Hello World')
+data = [
+  {
+    team: "Boston Bruins",
+    position: 1,
+    id: 1,
+  },
+  {
+    team: "Edmonton Oilers",
+    position: 2,
+    id: 2,
+  },
+  {
+    team: "New York Rangers",
+    position: 3,
+    id: 3,
+  },
+];
+
+app.get("/", (req, res) => {
+  res.json(data);
+});
+
+app.get("/team", (req, res) => {
+  res.status(204).send("Anaheim Ducks");
 });
 
 app.listen(port, () => {
-    console.log(`Server listening on port ${port}`)
+  console.log(`Server listening on port ${port}`);
 });
